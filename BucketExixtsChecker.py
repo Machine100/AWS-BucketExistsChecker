1ac74d8840ed11cd70ecde27a608de32bd4a3a0c
machine100 <joesmileyyy@yahoo.com>
2019-02-03 10:22:19 -0800

    Update BucketExixtsChecker.py

--- BucketExixtsChecker.py
+++ BucketExixtsChecker.py
@@ -1,12 +1,15 @@
-for word in words:
-	#bucket = s3.Bucket(word)     
-	exists = true     
-	try:
-		s3.meta.client.head_bucket('name')     
+import boto3
+import botocore
+s3 = boto3.client('s3')
 
-	except botocore.exceptions.ClientError as e:         
+bucket = s3.Bucket(word)     
+exists = true     
+try:
+	s3.meta.client.head_bucket('name')     
+
+except botocore.exceptions.ClientError as e:         
 		# if a client error is thrown, then check that it was a 404 error
 		# if it was a 404 error, then the bucket does not exist
-		error_code = e.response['Error']['Code']         
-		if error_code == '404':
-			exists=false
+	error_code = e.response['Error']['Code']         
+	if error_code == '404':
+		exists=false
