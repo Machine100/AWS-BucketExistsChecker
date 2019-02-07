b22fe8794a941fc1336437456f843df843f484b6
machine100 <joesmileyyy@yahoo.com>
2019-02-06 17:45:48 -0800

    Update BucketExixtsChecker.py

--- BucketExixtsChecker.py
+++ BucketExixtsChecker.py
@@ -2,8 +2,6 @@ import boto3
 import botocore
 s3resource = boto3.resource('s3')
 
-bucket = s3resource.Bucket('word')   #What does this do? Creates a Bucket Resource. Sub-resources are methods that create 
-                                     #a new instance of a child resource. This resource's identifiers get passed along to the child.  
 exists = True     
 try:
 	s3resource.meta.client.head_bucket(Bucket='mybucket')     
